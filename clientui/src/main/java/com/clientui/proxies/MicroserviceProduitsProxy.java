eb760146bbaee7abf0ee92702cac327576bcb1b4
Julien Deleligne <julien.deleligne@decathlon.com>
2021-04-23 10:56:30 +0200

    first commit

---
+++ clientui/src/main/java/com/clientui/proxies/MicroserviceProduitsProxy.java
@@ -0,0 +1,22 @@
+package com.clientui.proxies;
+
+import com.clientui.beans.ProductBean;
+import java.util.List;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "microservice-produits", url = "localhost:9001")
+public interface MicroserviceProduitsProxy {
+
+  @GetMapping(value = "/Produits")
+  List<ProductBean> listeDesProduits();
+
+  /*
+   * Notez ici la notation @PathVariable("id") qui est différente de celle qu'on utlise dans le contrôleur
+   **/
+  @GetMapping(value = "/Produits/{id}")
+  ProductBean recupererUnProduit(@PathVariable("id") int id);
+
+
+}
